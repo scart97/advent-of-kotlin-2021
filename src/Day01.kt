d1d77bf1d1914341e8401938f1140a79b8c3759c
scart97 <scart.lucas@gmail.com>
2021-12-01 01:52:41 -0300

    Implemented day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toLong()}
+        val differences = depths.zipWithNext { a, b -> b - a}
+        return differences.count { it > 0 }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toLong()}
+        val windows = depths.windowed(3)
+        val windowDepths = windows.map { it.sum() }
+        val differences = windowDepths.zipWithNext { a, b -> b - a}
+        return differences.count { it > 0 }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
